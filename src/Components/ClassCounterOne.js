ea033e6b245212219dc42634d46a6a7887d2d9f0
Srijan Shovit <srijanshovit@gmail.com>
2021-11-25 12:16:14 +0530

    conditionally run Effects

--- src/Components/ClassCounterOne.js
+++ src/Components/ClassCounterOne.js
@@ -4,7 +4,8 @@ class ClassCounterOne extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            count : 0
+            count : 0,
+            name:''
         }
     }
 
@@ -13,12 +14,24 @@ class ClassCounterOne extends Component {
     }
 
     componentDidUpdate(prevProps,prevState) {
-        document.title = `Clicked ${this.state.count} times`
+        //conditional updation
+        if (prevState.count !== this.state.count) {
+            console.log("Updating document title")
+
+            document.title = `Clicked ${this.state.count} times`
+        }
     }
     
     render() {
         return (
             <div>
+                <input
+                type='text'
+                value={this.state.name}
+                onChange={e => {
+                    this.setState({name:e.target.value})
+                }}
+                />
                 <button onClick={()=> this.setState
                 ({count : this.state.count + 1})
                 }>Count : {this.state.count}</button>
--- src/Components/HookCounterOne.js
+++ src/Components/HookCounterOne.js
@@ -2,14 +2,25 @@ import React,{useState,useEffect} from 'react'
 
 function HookCounterOne() {
     const [count,setCount] = useState(0)
+    const [name,setName] = useState('')
 
     //the parameter is a function which executes every time when component is rendered
     //useEffect is placed inside component so its state and props can easily be handled
     useEffect(()=> {
+        console.log("Updating document title")
         document.title = `Clicked ${count} times`
-    })
+        //useEffect takes second argument as a dependency array which takes state or peops
+        //which need to be watched and run useEffect only when that is updated
+    },[count])
     return (
         <div>
+            <input
+                type='text'
+                value={name}
+                onChange={e => 
+                    setName(e.target.value)
+                }
+                />
             <button onClick={()=> setCount(count+1)}>Count {count}</button>
         </div>
     )
